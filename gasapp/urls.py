ecca1d9e6bc5d253185bd8f1333c0a76f2a08394
Eduard Martinez <edu@chicfy.com>
2019-12-05 19:56:00 +0100

    mas

--- gasapp/urls.py
+++ gasapp/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 from stations.views import *
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', cache_page(60 * 60 * 24)(TemplateView.as_view(template_name='home.html')), name='home'),
+    path('about/', cache_page(60 * 60 * 24)(TemplateView.as_view(template_name='about.html')), name='about'),
     path('stations/', StationsView.as_view(), name="stations"),
+    path('offline/', cache_page(60 * 60 * 24)(TemplateView.as_view(template_name='offline.html')), name='offline'),
     path('admin/', admin.site.urls),
 ]
---
+++ stations/static/worker.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function isHtmlPage(event) {
+    return event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html');
+}
+
+self.addEventListener('install', function(event) {
+  var offlinePage = new Request('/offline/');
+  event.waitUntil(
+    fetch(offlinePage).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        return cache.put(offlinePage, response);
+      });
+  }));
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      return caches.open('offline').then(function(cache) {
+        return cache.match('/offline/');
+      });
+    }
+  ));
+});
+self.addEventListener('refreshOffline', function(response) {
+  return caches.open('offline').then(function(cache) {
+    return cache.put(offlinePage, response);
+  });
+});
