e0a31ff40aca3b8fbcbc6d605c8f54645b213e1c
Eduard Martínez Bernal <martinez.bernal@gmail.com>
2020-11-01 19:50:13 +0100

    usar dicts

--- stations/views.py
+++ stations/views.py
@@ -14,18 +14,20 @@ class StationsView(BaseListView):
         if 'center' in self.request.GET:
             lat, lng = self.request.GET.get('center').split(',')
             queryset = queryset.annotate(distance=Distance("location", Point(float(lat), float(lng), srid=4326))).order_by('distance')
-        return queryset
+        return queryset.values(
+            'pk', 'name', 'petrol95', 'petrol98', 'gasoil', 'address', 'city', 'postal_code', 'location', 'updated'
+        )
 
     def render_to_response(self, context):
         return JsonResponse({ 'stations': [[
-                s.pk,
-                s.name,
-                float(s.petrol95) if s.petrol95 else None,
-                float(s.petrol98) if s.petrol98 else None,
-                float(s.gasoil) if s.gasoil else None,
-                s.address,
-                s.city,
-                s.postal_code,
-                [coordinate for coordinate in s.location],
-                s.updated.timestamp()
+                s['pk'],
+                s['name'],
+                float(s['petrol95']) if s['petrol95'] else None,
+                float(s['petrol98']) if s['petrol98'] else None,
+                float(s['gasoil']) if s['gasoil'] else None,
+                s['address'],
+                s['city'],
+                s['postal_code'],
+                [coordinate for coordinate in s['location']],
+                s['updated'].timestamp()
             ] for s in context['object_list']]})
