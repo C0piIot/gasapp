623a64bc3af2c6e596356afe94da909c73f114d3
Eduard Martinez <martinez.bernal@gmail.com>
2022-09-25 16:16:54 +0200

    MM

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,7 @@
 if [[ ! -z "$SWAP" ]]; then fallocate -l 64M swapfile && chmod 600 swapfile && mkswap swapfile && swapon swapfile && ls -hla; fi;
 
 python manage.py migrate
+sqlite3 /code/db.sqlite3 < /code/pragma.sql
+
 while true; do python manage.py update_prices; echo "Prices updated"; sleep 3600; done&
 uwsgi /code/docker/uwsgi.ini
